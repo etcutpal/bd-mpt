4f578598adc0bb881cb7273bfa25dce7bfe8d9a4
Utpal Mandal <etcutpal@gmail.com>
2026-04-26 00:01:46 +0600

    Vercel JSON

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const XLSX = require('xlsx-js-style');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
